0469b693405128e273bbafe5de0210698f82f185
wagtail616 <109149349+wagtail616@users.noreply.github.com>
2023-12-06 16:36:33 +0900

    StaticTyping

---
+++ StaticTyping/StaticTyping.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	using std::cout;
+	using std::endl;
+	int num = 42;
+	double pi = 3.14159;
+
+	num = pi;//ˆÃŽ¦“IŒ^•ÏŠ·
+
+	cout << "num = " << num << endl;
+	cout << "pi = " << pi << endl;
+}
