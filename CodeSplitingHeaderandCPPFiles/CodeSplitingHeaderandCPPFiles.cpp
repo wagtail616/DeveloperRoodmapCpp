57949aafb3c314dd94b8161125dacce8634b825d
wagtail616 <109149349+wagtail616@users.noreply.github.com>
2023-12-08 19:39:39 +0900

    CodeSplitingHeaderandCPPFiles完了

---
+++ CodeSplitingHeaderandCPPFiles/CodeSplitingHeaderandCPPFiles.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"example.h"
+
+void Example::printMessage() {
+	std::cout << "Hello, code splitting!" << std::endl;
+}
+int main() {
+}
---
+++ CodeSplitingHeaderandCPPFiles/example.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+class Example {
+public:
+	void printMessage();
+};
+
+#endif
