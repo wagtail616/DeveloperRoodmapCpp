1f20837e55b27014ca8dddd6c9beaa4de54fbdd9
wagtail616 <109149349+wagtail616@users.noreply.github.com>
2023-12-10 16:51:58 +0900

    呼び出し方の記述

--- CodeSplitingHeaderandCPPFiles/CodeSplitingHeaderandCPPFiles.cpp
+++ CodeSplitingHeaderandCPPFiles/CodeSplitingHeaderandCPPFiles.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
 #include"example.h"
+#include<memory>
 
 void Example::printMessage() {
 	std::cout << "Hello, code splitting!" << std::endl;
 }
 int main() {
+	//普通に宣言
+	//スタックに積まれる
+	Example obj;
+	obj.printMessage();
+
+	//ポインタ
+	//ヒープに詰まれる
+	Example* ptr = new Example();
+	ptr->printMessage();
+	delete ptr;
+
+	//ポインタ
+	//スタックに詰まれる
+	//newとdeleteがいらない
+	Example* obj_ptr = &obj;
+	obj_ptr->printMessage();
+
+	//スマートポインタ
+	//スタックに積まれる
+	 std::unique_ptr<Example> unique= std::unique_ptr<Example>();
+	//あるいは　 
+	//auto unique = std::unique_ptr<Example>();
+	unique->printMessage();
 }
