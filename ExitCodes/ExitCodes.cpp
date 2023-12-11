57c51dc13f31d245f332321194ff6affb539fcc5
wagtail616 <109149349+wagtail616@users.noreply.github.com>
2023-12-11 17:29:35 +0900

    ExitCods完了

---
+++ ExitCodes/ExitCodes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+double division(double a , double b) {
+	double result;
+	result = a / b;
+	return result;
+}
+
+int main() {
+	double a, b;
+	std::cout << "Enter two numbers for division:\n";
+	std::cin >> a >> b;
+	if (b==0) {
+		std::cout << "Division by zero!";
+		std::exit(1);
+		std::cout << "Division by zero!";
+	}
+	
+	std::cout << "a / b = "<< division(a,b);
+	return 0;
+	std::cout << "‚±‚±‚ÍŽÀs‚³‚ê‚È‚¢" << division(a, b);
+
+}
