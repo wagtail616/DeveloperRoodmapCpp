702ca416b03f0ad20d2a1313b9be068c15f77580
wagtail616 <109149349+wagtail616@users.noreply.github.com>
2023-12-11 16:56:21 +0900

    DiamondInheritance完了

--- DiamondInheritance/Diamondinheritance.cpp
+++ DiamondInheritance/Diamondinheritance.cpp
@@ -9,7 +9,33 @@ public:
 
 class Derived1 :virtual public Base {
 public:
-	void derived1print() {
+	void derived1Print() {
 		std::cout << "Derived1 class" << std::endl;
 	}
 };
+
+class Derived2 : virtual public Base {
+public:
+	void derived2Print() {
+		std::cout << "Derived2 class" << std::endl;
+	}
+};
+
+
+
+class Derived3 : virtual public Base,public Derived1,public Derived2 {
+public:
+	void derived3Print() {
+		std::cout << "Derived3 class" << std::endl;
+	}
+};
+
+int main() {
+	Derived3 d3;
+	d3.print();
+	d3.derived1Print();
+	d3.derived2Print();
+	d3.derived3Print();
+
+	return 0;
+}
