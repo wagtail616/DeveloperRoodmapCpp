0232f26227d58370907897e2c1db1df348574ccf
wagtail616 <109149349+wagtail616@users.noreply.github.com>
2023-12-13 16:30:36 +0900

    AutoTypeDeducation完了

--- AutoTypeDeducation/AutoTypeDeducation.cpp
+++ AutoTypeDeducation/AutoTypeDeducation.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
-#include<Vector>
+#include<vector>
+
+auto add(int x, int y) {
+	return x + y;
+}
 
 int main() {
 	int myInt = 5;
@@ -7,8 +11,17 @@ int main() {
 	//本題
 	auto myAutoInt = 5;
 	
-	//
-	std::vecter<int> myVector = { 1,2,3,4,5 };
+	//ベクター
+	std::vector<int>myVector = { 1,2,3,4,5 };
 
+	//auto にベクターを代入
+	for (auto it = myVector.begin(); it != myVector.end(); ++it) {
+		std::cout << *it << std::endl;
+	}
+
+	//auto型は何らかの型で初期化しないとエラー
+	//auto myvar;
+
+	
 
 }
