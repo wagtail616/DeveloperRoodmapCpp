e9de440627ff6e7dfc5dfad9e5e9cc6f53550dec
wagtail616 <109149349+wagtail616@users.noreply.github.com>
2023-12-13 17:09:24 +0900

    コミット忘れ

--- TypeCasting/TypeCasting.cpp
+++ TypeCasting/TypeCasting.cpp
@@ -12,8 +12,19 @@ int main(){
 	//スタティックキャスティング
 	float c = static_cast<float>(a);
 
+	//ダイナミックキャスト
 	Base* base_ptr = new Derived();
 	Derived* derived_ptr = dynamic_cast<Derived*>(base_ptr);
 	delete base_ptr;
+
+	//reinterpretCast
+	int* d = new int(42);
+	long e = reinterpret_cast<long>(d);
+	delete d;
+	
+	//const cast
+	const int a = 10;
+	int* ptr = const_cast<int*>(&a);
+	*ptr = 20;
 
 }
